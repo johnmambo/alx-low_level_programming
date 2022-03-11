9fb3f40b18c081fe80005a77adecdc58bdb8071e
johnmambo <johnmambo794@gmail.com>
2022-03-11 09:01:51 -0800

    hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - main block
+* Return: 0
+*/
+int main(void)
+{
+char c;
+char d = '0';
+while (d <= '9')
+{
+putchar(d);
+d++;
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
