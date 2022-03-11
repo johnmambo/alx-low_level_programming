87f48e93577c15c31e62462baa336582b78ecd3d
johnmambo <johnmambo794@gmail.com>
2022-03-11 05:47:15 -0800

    reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - main block
+*Description: prints all single digit numbers of base 10
+*
+*starting from 0, followed by a new line.
+*Return: 0
+*/
+int main(void)
+{
+char i = 'z';
+while (i >= 'a')
+{
+putchar(i);
+i--;
+}
+putchar('\n');
+return (0);
+}
