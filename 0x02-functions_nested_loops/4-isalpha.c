dc1700caa96a3d4628ea76d8000d301054535bae
johnmambo <82521915+johnmambo@users.noreply.github.com>
2022-03-15 14:21:06 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,13 @@
 #include "main.h"
+
 /**
-* _isalpha - check for lower case letter
-* @c : character to check
-* Return:0 or 1
-*/
+ * _isalpha - check for lower case letter
+ * @c : character to check
+ * Return:0 or 1
+ */
+
 int _isalpha(int c)
 {
-return ((c >= 99 && c <= 122) || (c <= 65 && c <= 90));
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+
 }
