5e951e15fc68ef675ab8860d03a3b921459a88d8
johnmambo <johnmambo794@gmail.com>
2022-03-17 04:42:59 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*
+* print_most_numbers - prints numbers
+* Return :0
+*/
+
+void print most_numbers(void)
+{
+char d = 0;
+while (d =< 9)
+{
+if (d != 2 && d != 4)
+{
+_putchar('0' + d);
+}
+d++; 
+_putchar('\n');
+}
+return (0);
+}
+ 
