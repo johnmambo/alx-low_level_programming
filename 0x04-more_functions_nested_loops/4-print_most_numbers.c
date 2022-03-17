926bec563481c381faa93b86074001befe282f03
johnmambo <johnmambo794@gmail.com>
2022-03-17 04:49:33 -0700

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,19 +7,15 @@
 
 void print_most_numbers(void)
 {
-	char n = 0;
-
-	while (n <= 9)
-	{
-		if (n != 2 && n != 4)
-		{
-			_putchar('0' + n);
-		}
-
-		n++;
-	}
-	_putchar('\n');
-
+char n = 0;
+while (n <= 9)
+{
+if (n != 2 && n != 4)
+{
+_putchar('0' + n);
 }
-
- 
+n++;
+}
+_putchar('\n');
+return (0);
+}
