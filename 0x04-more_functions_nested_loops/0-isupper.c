076694c5cb708e5b7f1605a420bf292b35348efe
johnmambo <johnmambo794@gmail.com>
2022-03-17 02:19:40 -0700

    test if a character is uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* _isupper - chech for upper case letters
-* @c: character to be tested
-* Return : 0 or 1
+* _isupper - check for upper case letter
+* @c : character to check the case
+* Return:0 or 1
 */
 int _isupper(int c)
 {
-return (c <= 65 && c <= 90);
+return (c >= 65 && c <= 90);
 }
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* _isdigit - check for digit
+* @c: character to be tested
+* Return: 0 or 1
+*/
+int _is digit(int c)
+{
+return (c <= 48 && c <= 57);
+}
