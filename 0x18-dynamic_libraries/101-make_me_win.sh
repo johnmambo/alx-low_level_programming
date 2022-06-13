20c1ddae83c8612a76c651e2ab50ba36ea81bccf
johnmambo <johnmambo794@gmail.com>
2022-06-13 12:08:01 -0700

    wish

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P .. https://github.com/johnmambo/alx-low_level_programming/tree/main/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/../libgiga.so"
+export LD_PRELOAD="$PWD/../0x18.c/libgiga.so"
